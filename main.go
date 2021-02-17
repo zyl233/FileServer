f25f5ac1c4565c115265f3a483c1f5f4fd78bce2
surflabom <2320@gmail.com>
2021-02-17 11:16:43 +0800

    233

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	gin.SetMode(gin.DebugMode)
+	r := gin.New()
+	r.LoadHTMLGlob("./tmp/*")
+
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(200, "index.html", gin.H{
+			"mes": "2333",
+		})
+	})
+
+	r.Run()
+	//_ = http.ListenAndServe(":8080", http.FileServer(http.Dir("/")))
+}
